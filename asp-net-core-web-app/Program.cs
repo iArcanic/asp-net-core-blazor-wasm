b79da76f8e9a554d2d97ba517b89319e2ee5f7ac
Preetham Ananthkumar <preetham.mervin@gmail.com>
2023-12-15 16:40:49 +0000

    Added project references, installed relevant Nuget package and added config lines

--- asp-net-core-web-app/Program.cs
+++ asp-net-core-web-app/Program.cs
@@ -12,6 +12,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+else
+{
+    app.UseWebAssemblyDebugging();
+}
+
+app.UseBlazorFrameworkFiles();
+app.MapFallbackToFile("index.html");
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
